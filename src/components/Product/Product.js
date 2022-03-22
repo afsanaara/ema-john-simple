9d998f62fdeb60c84c472325adadadbc382b4caa
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-22 23:47:51 +0600

    commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
     const {name, img, seller,price,ratings} =props.product;
@@ -8,7 +9,10 @@ const Product = (props) => {
            <div className="product-info">
                <p className="product-name">{name}</p>
                <p>Price:{price}</p>
+               <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
            </div>
+           <button className='btn-cart'>Add to cart</button>
         </div>
     );
 };
