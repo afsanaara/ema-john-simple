a8dd4e9139bd172f40c702ae41773e6123a25de0
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-03-23 12:52:32 +0600

    new commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Product.css'
 
@@ -12,7 +14,11 @@ const Product = ({product,handleAddToCart}) => {
                <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
            </div>
-           <button onClick={()=>handleAddToCart(product)} className='btn-cart'>Add to cart</button>
+           <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
+              <p className='btn-text'>Add to cart</p> 
+              <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+               </button>
+           
         </div>
     );
 };
